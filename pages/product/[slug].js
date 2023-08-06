0b266017a92cf90a21f4b33381bd7d137208f0a7
Sam Lalli <samlalli@Sams-MacBook-Pro.local>
2023-08-06 00:45:38 -0600

    addToCart handler created

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,20 +1,35 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Layout from '../../components/Layout'
 import { useRouter } from 'next/router'
 import data from '../../utils/data';
 import Link from 'next/link';
 import Image from 'next/image';
+import { Store } from '../../utils/Store';
 
 export default function ProductScreen() {
+    const { state, dispatch } = useContext(Store);
     const { query } = useRouter();
     const { slug } = query
     const product = data.products.find(x => x.slug === slug)
     if(!product){
         return <div>Product Not Found</div>
     }
+
+    const addToCart = () => {
+        const existItem = state.cart.cartItems.find((x) => x.slug === product.slug);
+        const quantity = existItem ? existItem.quantity + 1 : 1;
+
+        if(product.countInStock < quantity) {
+            alert('Sorrt, this product is out of stock');
+            return;
+        }
+
+        dispatch({ type: 'CART_ADD_ITEM' , payload: { ...product, quantity: quantity} });
+    };
+
   return (
     <Layout title={product.name}>
-        <div>
+        <div className='py-2'>
             <Link href={'/'}>Back to Products</Link>
         </div>
         <div className='grid md:grid-cols-4 md:gap-3'>
@@ -47,7 +62,7 @@ export default function ProductScreen() {
                         <div>Status</div>
                         <div>{product.countInStock > 0 ? 'In stock' : 'Unavailable'}</div>
                     </div>
-                    <button className='primary-button w-full'>Add to cart</button>
+                    <button className='primary-button w-full' onClick={addToCart}>Add to cart</button>
                 </div>
             </div>
         </div>
