72729b4edef49f780c61be8e14a07bcecbfcfe65
Sam Lalli <samlalli@Sams-MacBook-Pro.local>
2023-08-28 21:10:25 -0600

    login api created, logout pending

--- components/Layout.js
+++ components/Layout.js
@@ -2,8 +2,14 @@ import Head from 'next/head';
 import Link from 'next/link';
 import React, { useContext, useEffect, useState } from 'react';
 import { Store } from '../utils/Store';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { useSession } from 'next-auth/react';
 
 export default function Layout({ title, children }) {
+
+  const { status, data: session } = useSession();
+
 
   const { state } = useContext(Store);
   const { cart } = state;
@@ -18,6 +24,8 @@ export default function Layout({ title, children }) {
         <meta name="description" content="Ecommerce Website" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      <ToastContainer position='bottom-center' limit={1} />
 
       <div className="flex min-h-screen flex-col justify-between">
         <header>
@@ -35,9 +43,17 @@ export default function Layout({ title, children }) {
                 )}
                 </a>
               </Link>
-              <Link href="/login">
-                <a className="p-2">Login</a>
-              </Link>
+
+                {status === 'loading' ? (
+                  'Loading'
+                  ) : session?.user ? ( 
+                    session.user.name 
+                  ) : (
+                  <Link href="/login">
+                  <a className="p-2">Login</a>
+                </Link>
+                )}
+
             </div>
           </nav>
         </header>
